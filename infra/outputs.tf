c367e70dbece08fbe704b848c353783651b814e0
MJ Abadilla <mjmaix@gmail.com>
2022-11-15 01:09:35 +0800

    more output information

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -12,3 +12,33 @@ output "function_name" {
   description = "Name of the Lambda function."
   value       = aws_lambda_function.function.function_name
 }
+
+output "backend_dynamodb_table_arn" {
+  description = "Backend DynamoDB table ARN"
+  value       = module.terraform_state_backend.dynamodb_table_arn
+}
+output "backend_dynamodb_table_id" {
+  description = "Backend DynamoDB table ID"
+  value       = module.terraform_state_backend.dynamodb_table_id
+}
+output "backend_dynamodb_table_name" {
+  description = "Backend DynamoDB table name"
+  value       = module.terraform_state_backend.dynamodb_table_name
+}
+
+output "backend_s3_bucket_arn" {
+  description = "Backend S3 bucket ARN"
+  value       = module.terraform_state_backend.s3_bucket_arn
+}
+output "backend_s3_bucket_domain_name" {
+  description = "Backend S3 bucket domain name"
+  value       = module.terraform_state_backend.s3_bucket_domain_name
+}
+output "backend_s3_bucket_id" {
+  description = "Backend S3 bucket ID"
+  value       = module.terraform_state_backend.s3_bucket_id
+}
+output "backend_terraform_backend_config" {
+  description = "Backend Rendered Terraform backend config file"
+  value       = module.terraform_state_backend.terraform_backend_config
+}
